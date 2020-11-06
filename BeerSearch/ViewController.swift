15cd8c89e1c1c9be5ca677eb163a4f44cf1879b6
jsainz <jsainz@ayesa.com>
2020-11-06 13:47:47 +0100

    Initial Commit

---
+++ BeerSearch/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  BeerSearch
+//
+//  Created by Joaquin Sainz de la Maza on 2020-11-06.
+//  Copyright © 2020 Joaquin Sainz de la Maza. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
